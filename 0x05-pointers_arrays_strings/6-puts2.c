c2bb1ea5160df045051d09aaf48172838880f8a6
Badr El Abdellaoui <b.elabdellaoui101@gmail.com>
2024-01-02 09:36:40 -0800

    6 correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,14 @@
 void	puts2(char *str)
 {
 	int	i;
+	int	len;
 
-	i = 0;
-	while (str[i])
+	len = 0;
+	while (str[len])
+		len++;
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
+	_putchar('\n');
 }
