9ac33a1d17ee6824ee5f689a6a4f5c81f338d394
therealBadr <b.elabdellaoui101@gmail.com>
2024-02-18 11:47:53 -0800

    0x0C: 0 Fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocate memory.
+ * b - size to be allocated
+ * 
+ * Return: adress to the pointer
+ */
+
 void	*malloc_checked(unsigned int b)
 {
 	char	*ptr;
